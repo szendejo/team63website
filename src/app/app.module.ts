0779842fa550197296691239e4dba12089fc2d03
Stephanie <szacademia@gmail.com>
2021-02-11 10:02:55 -0600

    add more updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { CaseStudyComponent } from './case-study/case-study.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CaseStudyComponent
   ],
   imports: [
-    BrowserModule
+    appRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
